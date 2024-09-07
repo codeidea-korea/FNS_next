75419f9ae902159d708c04a3470cdfcaab5c537c
pintoll <serendipity8271@gmail.com>
2024-09-07 17:52:58 +0900

    feat: production

--- src/common/AxiosInstance.jsx
+++ src/common/AxiosInstance.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_VITE_DEV_API_URL}`, // 개발 API SERVER
-  // baseURL: `${process.env.NEXT_PUBLIC_VITE_PROD_API_URL}`, // 운영 API SERVER
+  // baseURL: `${process.env.NEXT_PUBLIC_VITE_DEV_API_URL}`, // 개발 API SERVER
+  baseURL: `${process.env.NEXT_PUBLIC_VITE_PROD_API_URL}`, // 운영 API SERVER
   headers: {
     "system-key": `${process.env.NEXT_PUBLIC_VITE_SYSTEM_KEY}`,
   },
