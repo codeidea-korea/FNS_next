bf85760589a24f8eaa2d77761a4aa3b60e66a922
h0ngg0m <jaehongbyeon98@gmail.com>
2024-09-11 13:47:53 +0900

    :mag: 마이 페이지 seo

--- src/app/mypage/layout.jsx
+++ src/app/mypage/layout.jsx
@@ -1,11 +1,5 @@
 import BaseLayout from "@/layout/BaseLayout";
 
-export const metadata = {
-  title: "마이페이지 | 패션앤스타일 (Fashion & Style)",
-  description:
-    "마이페이지 | 패션앤스타일(Fashion & Style)이 제공하는 최신 트렌드 패션 아이템과 다양한 셀럽들의 스타일을 만나보세요.",
-};
-
 export const generateViewport = () => {
   return {
     width: "device-width",
--- src/app/mypage/page.jsx
+++ src/app/mypage/page.jsx
@@ -1,4 +1,13 @@
 import Mypage from "@/page/mypage/Mypage";
+import { makeMetadata } from "@/utils/metadata";
+
+export async function generateMetadata() {
+  return makeMetadata(
+    "마이페이지 | 패션앤스타일 (Fashion & Style)",
+    "패션앤스타일(Fashion & Style)이 제공하는 최신 트렌드 패션 아이템과 다양한 셀럽들의 스타일을 만나보세요.",
+    "https://www.fashionandstyle.com/mypage",
+  );
+}
 
 export default function Page() {
   return <Mypage />;
