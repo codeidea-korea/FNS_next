b3417e5d4e40c71f40524b006520ad51a8a3aa18
h0ngg0m <jaehongbyeon98@gmail.com>
2024-09-23 10:32:03 +0900

    :hammer: /home/tag/[key]/[key] 페이지 description을 /tag 페이지와 동일하게 수정

--- src/app/home/tag/[key1]/[key2]/page.tsx
+++ src/app/home/tag/[key1]/[key2]/page.tsx
@@ -1,23 +1,12 @@
 import Home from "@/page/main/Home";
 import { getMetaInfo, makeMetadata } from "@/utils/metadata";
 import { Metadata } from "next";
+import { clearMetaText } from "@/utils/common";
+import { getApi } from "@/utils/apis";
+import { redirect } from "next/navigation";
 
 interface Props {
   params: { key1: string; key2: string };
-}
-
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const originKey = decodeURIComponent(params.key2);
-
-  const { title, description } = getMetaInfo(originKey);
-  const url = decodeURIComponent(
-    "https://www.fashionandstyle.com/home/tag/" +
-      params.key1 +
-      "/" +
-      params.key2,
-  );
-
-  return makeMetadata(title, description, url);
 }
 
 export function generateViewport(): { width: string; initialScale: number } {
@@ -25,6 +14,46 @@ export function generateViewport(): { width: string; initialScale: number } {
     width: "device-width",
     initialScale: 1,
   };
+}
+
+function getMetaDescription(tagPreview: TagPreview): string {
+  let metaDesc = "";
+  if (tagPreview && tagPreview.vw_groups?.length > 0) {
+    const top5Data = tagPreview.vw_groups[1].grp_items[0].itm_data;
+
+    top5Data.map((topData) => {
+      metaDesc = metaDesc + topData.post_desc?.split("\n")[0] + " ";
+    });
+
+    return clearMetaText(metaDesc);
+  }
+  return metaDesc;
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  try {
+    const { data } = await getApi<TagPreview>(
+      `/api/v1/ui/view/tag_preview_name/${params.key2}`,
+    );
+    const metaDescription = data.data ? getMetaDescription(data.data) : "";
+
+    return makeMetadata(
+      decodeURIComponent(params.key2) + " | 패션앤스타일 (Fashion & Style)" ??
+        "",
+      clearMetaText(metaDescription ?? ""),
+      decodeURIComponent(
+        "https://www.fashionandstyle.com/home/tag/" +
+          params.key1 +
+          "/" +
+          params.key2,
+      ),
+      data.data?.vw_groups[1]?.grp_items[0]?.itm_data[0]?.image_url_def ??
+        data.data?.vw_groups[0]?.grp_items[0]?.itm_data[0]?.image_url1,
+      data.data?.created_at ?? "",
+    );
+  } catch (error) {
+    redirect("/");
+  }
 }
 
 export default function Page() {
